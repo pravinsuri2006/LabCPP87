69d76acaa0ed9fcfd31acb039a0b321a4463480d
pravinsuri2006 <pravinsuri2006@gmail.com>
2026-07-21 09:00:24 +0000

    sum and average program in lab

---
+++ sum.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num1,num2,sum,avg;
+    cout<<"Enter Two Numbers";
+    cin>>num1;
+    cin>>num2;
+    sum=num1+num2;
+    avg=sum/2;
+    cout<<"sum="<<sum<<" \t average="<<avg;
+    return 0;
+
+}
