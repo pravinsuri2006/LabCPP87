c6b3695ecaa3237976777b327a046540a5942b13
pravinsuri2006 <pravinsuri2006@gmail.com>
2026-07-21 09:54:18 +0000

    add sub

--- file1.cpp
+++ file1.cpp
@@ -9,5 +9,12 @@
         std::cout<<"sum of two number is"<<(a+b);
         return 0;
     }
-    
+    int sub()
+    {
+        int a,b,sub;
+        std::cout<<"enter the two number";
+        std::cin>>a>>b;
+        std::cout<<"sub of two number is"<<(a-b);
+        return 0;
+    }
  }
