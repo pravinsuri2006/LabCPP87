9c20aa66e6da26350631c86dfa41891b3a4c73a1
pravinsuri2006 <pravinsuri2006@gmail.com>
2026-07-21 09:48:21 +0000

    2 file separate vfile running toghy

--- file1.cpp
+++ file1.cpp
@@ -1,12 +1,13 @@
 #include<iostream>
- namespace sycs4;
+ namespace sycs4
  {
     int add()
     {
         int a,b,sum;
-        cout<<"enter the two number";
-        cin>>a>>b;
-        cout<<"sum of two number is"<<(a=b);
+        std::cout<<"enter the two number";
+        std::cin>>a>>b;
+        std::cout<<"sum of two number is"<<(a+b);
         return 0;
     }
+    
  }
