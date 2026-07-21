9e5c9ec2b5c4bb136b7440f636994f4a4ec158c9
pravinsuri2006 <pravinsuri2006@gmail.com>
2026-07-21 09:29:03 +0000

    commit

---
+++ file1.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+ namespace sycs4;
+ {
+    int add()
+    {
+        int a,b,sum;
+        cout<<"enter the two number";
+        cin>>a>>b;
+        cout<<"sum of two number is"<<(a=b);
+        return 0;
+    }
+ }
